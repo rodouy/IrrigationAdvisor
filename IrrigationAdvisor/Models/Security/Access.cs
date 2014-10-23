1642a92c1e3d87698f327f377603f4ca69284058
monicarle <monicacarle@gmail.com>
2014-10-23 19:18:05 -0200

    Access class

---
+++ IrrigationAdvisor/Models/Security/Access.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IrrigationAdvisor.Models.Security
+{
+    class Access
+    {
+        /// <summary>
+        /// Create: 2014-10-21
+        /// Author: rodouy - monicarle
+        /// Description: 
+        ///     Describes a kind of access
+        ///     
+        /// References:
+        ///     list of classes this class use
+        ///     
+        /// Dependencies:
+        ///     list of classes is referenced by this class
+        /// 
+        /// TODO: OK
+        ///     UnitTest
+        ///     
+        /// -----------------------------------------------------------------
+        /// Fields of Class:
+        ///     - name String
+        /// 
+        /// Methods:
+        ///     - ClassTemplate()      -- constructor
+        ///     - ClassTemplate(name)  -- consturctor with parameters
+        ///     - SetName(newName)     -- method to set the name field
+        /// 
+        /// </summary>
+        /// 
+
+        public Access() { }
+    }
+}
