afe4991663545bc62fbbb4f02799ed44a481bdbf
monicarle <monicacarle@gmail.com>
2014-11-08 22:16:11 -0200

    clases perdidas

---
+++ IrrigationAdvisor/Models/Management/IrrigationRecords.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IrrigationAdvisor.Models.Management
+{
+    /// <summary>
+    /// Create: 2014-10-31
+    /// Author: monicarle
+    /// Description: 
+    ///     Contains all the daily records of a crop 
+    ///     and knows the last state of the crop
+    ///     
+    /// References:
+    ///     CropIrrigationWeather
+    ///     DailyRecord
+    ///     DateTime
+    ///     
+    /// Dependencies:
+    ///     IrrigationSystem
+    /// 
+    /// TODO: 
+    ///     UnitTest
+    ///     
+    /// -----------------------------------------------------------------
+    /// Fields of Class:
+    ///     - cropIrrigationWeather: CropIrrigationWeather
+    ///     - dailyRecords: List<DailyRecord>
+    ///     - growingDegreeDays: double
+    ///     - modifiedGrowingDegreeDays: double
+    ///     - totalEvapotranspirationCrop: double
+    ///     - totalEffectiveRain: double
+    ///     - hydricBalance: double
+    ///     - soilHydricVolume: double
+    ///     - lastWaterInput: Date
+    ///     - totalEvapotranspirationCropFromLastWaterInput: double
+    /// 
+    /// Methods:
+    ///     - IrrigationRecords()      -- constructor
+    ///     - IrrigationRecords(name)  -- consturctor with parameters
+    ///     - getPhenologicalStage(DayGrades: double): Stage
+    ///     - setGrowingDegreeDays(): bool
+    ///     - setPhenologicalState(Stage): bool
+    ///     - setNewPhenologicalStage(Stage): GrowingDegreeDays: double
+    ///     - getRootDepth(Specie, Stage): double
+    ///     - getPhenologicalStage(GrowingDegreeDays: double): Stage
+    ///     - getAvailableWater(RootDepth:double, FieldCapacity:double, PermanentWiltingPoint:double): double
+    ///     - getHydricBalance(): double
+    ///     - getSoilHydricVolume(): double
+    ///     - getEffectiveRain(Date): double
+    ///     - getGrowingDegree(Date): double
+    ///     - getEvapotranspirationCrop(Date): double
+    ///     - getObservations(Date): String
+    ///     - getLastThreeDaysOfEvapotranspirationCrop(): double
+    ///     
+    /// </summary>
+    public class IrrigationRecords
+    {
+        #region Consts
+        #endregion
+
+        #region Fields
+
+        private CropIrrigationWeather cropIrrigationWeather;
+        private List<DailyRecord> dailyRecords;
+        private double growingDegreeDays;
+        private double modifiedGrowingDegreeDays;
+        private double totalEvapotranspirationCrop;
+        private double totalEffectiveRain;
+        private double hydricBalance;
+        private double soilHydricVolume;
+        private DateTime lastWaterInput;
+        private double totalEvapotranspirationCropFromLastWaterInput;
+
+        #endregion
+
+        #region Properties
+        public CropIrrigationWeather CropIrrigationWeather
+        {
+            get { return cropIrrigationWeather; }
+            set { cropIrrigationWeather = value; }
+        }
+ 
+        public List<DailyRecord> DailyRecords
+        {
+            get { return dailyRecords; }
+            set { dailyRecords = value; }
+        }
+ 
+        public double GrowingDegreeDays
+        {
+            get { return growingDegreeDays; }
+            set { growingDegreeDays = value; }
+        }
+
+        public double ModifiedGrowingDegreeDays
+        {
+            get { return modifiedGrowingDegreeDays; }
+            set { modifiedGrowingDegreeDays = value; }
+        }
+
+        public double TotalEvapotranspirationCrop
+        {
+            get { return totalEvapotranspirationCrop; }
+            set { totalEvapotranspirationCrop = value; }
+        }
+
+        public double TotalEffectiveRain
+        {
+            get { return totalEffectiveRain; }
+            set { totalEffectiveRain = value; }
+        }
+ 
+        public double HydricBalance
+        {
+            get { return hydricBalance; }
+            set { hydricBalance = value; }
+        }
+
+        public double SoilHydricVolume
+        {
+            get { return soilHydricVolume; }
+            set { soilHydricVolume = value; }
+        }
+
+        public DateTime LastWaterInput
+        {
+            get { return lastWaterInput; }
+            set { lastWaterInput = value; }
+        }
+        
+        public double TotalEvapotranspirationCropFromLastWaterInput
+        {
+            get { return totalEvapotranspirationCropFromLastWaterInput; }
+            set { totalEvapotranspirationCropFromLastWaterInput = value; }
+        }
+
+        #endregion
+
+        #region Construction
+
+        public IrrigationRecords() 
+        {
+            this.CropIrrigationWeather = new CropIrrigationWeather();
+            this.DailyRecords = new List<DailyRecord>();
+            this.GrowingDegreeDays = 0;
+            this.ModifiedGrowingDegreeDays = 0;
+            this.TotalEvapotranspirationCrop = 0;
+            this.TotalEffectiveRain = 0;
+            this.HydricBalance = 0;
+            this.SoilHydricVolume = 0;
+            this.LastWaterInput = DateTime.Now;
+            this.TotalEvapotranspirationCropFromLastWaterInput = 0;
+            
+        }
+
+        public IrrigationRecords(CropIrrigationWeather pCropIrrigationWeather, 
+            List <DailyRecord> pDailyRecords, double pGrowingDegreeDays, 
+            double pModifiedGrowingDegreeDays, double pTotalEvapotranspirationCrop,
+            double pTotalEffectiveRain, double pHidricBalance, double pSoilHidricVolume,
+            DateTime pLastWaterInput, double pTotalEvapotranspirationCropFromLastWaterInput) 
+        {
+            this.CropIrrigationWeather = pCropIrrigationWeather;
+            this.DailyRecords = pDailyRecords;
+            this.GrowingDegreeDays = pGrowingDegreeDays;
+            this.ModifiedGrowingDegreeDays = pModifiedGrowingDegreeDays;
+            this.TotalEvapotranspirationCrop = pTotalEvapotranspirationCrop;
+            this.TotalEffectiveRain = pTotalEffectiveRain;
+            this.HydricBalance = pHidricBalance;
+            this.SoilHydricVolume = pSoilHidricVolume;
+            this.LastWaterInput = pLastWaterInput;
+            this.TotalEvapotranspirationCropFromLastWaterInput = pTotalEvapotranspirationCropFromLastWaterInput;
+        
+        }
+
+        #endregion
+
+        #region Private Helpers
+        #endregion
+
+        #region Public Methods
+
+        ///     - getPhenologicalStage(DayGrades: double): Stage
+        ///     - setGrowingDegreeDays(): bool
+        ///     - setPhenologicalState(Stage): bool
+        ///     - setNewPhenologicalStage(Stage): GrowingDegreeDays: double
+        ///     - getRootDepth(Specie, Stage): double
+        ///     - getPhenologicalStage(GrowingDegreeDays: double): Stage
+        ///     - getAvailableWater(RootDepth:double, FieldCapacity:double, PermanentWiltingPoint:double): double
+        ///     - getHydricBalance(): double
+        ///     - getSoilHydricVolume(): double
+        ///     - getEffectiveRain(Date): double
+        ///     - getGrowingDegree(Date): double
+        ///     - getEvapotranspirationCrop(Date): double
+        ///     - getObservations(Date): String
+        ///     - getLastThreeDaysOfEvapotranspirationCrop(): double
+
+
+
+        #endregion
+
+        #region Overrides
+        #endregion
+    }
+}
---
+++ IrrigationAdvisor/Models/Water/EvapotranspirationCrop.cs
@@ -0,0 +1,79 @@
+﻿using IrrigationAdvisor.Models.Management;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IrrigationAdvisor.Models.Water
+{
+    /// <summary>
+    /// Create: 2014-10-30
+    /// Author: monicarle
+    /// Description: 
+    ///     Describes the EvapoTranspiration over a Crop
+    ///     
+    /// References:
+    ///     WaterOutput
+    ///     
+    /// Dependencies:
+    ///     
+    /// 
+    /// TODO: 
+    ///     UnitTest
+    ///     
+    /// -----------------------------------------------------------------
+    /// Fields of Class:
+    ///     - type String
+    /// 
+    /// Methods:
+    ///     - EvapotranspirationCrop()      -- constructor
+    ///     - GetType()
+    /// 
+    /// </summary>
+    public class EvapotranspirationCrop: WaterOutput
+    {
+        #region Consts
+
+        private String TYPE = "EVAPOTRANSPIRATION";
+
+        #endregion
+
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Construction
+        public EvapotranspirationCrop() 
+        {
+
+        }
+
+        public EvapotranspirationCrop(CropIrrigationWeather pCropIrrigationWeather, DateTime pDate, double pInput)
+        {
+            this.CropIrrigationWeather = pCropIrrigationWeather;
+            this.Date = pDate;
+            this.Input = pInput;
+
+        }
+
+
+        #region Private Helpers
+        #endregion
+
+        #region Public Methods
+
+        public String getInputType()
+        {
+            return this.TYPE;
+        }
+        #endregion
+
+            #region Overrides
+            #endregion
+
+        #endregion
+
+    }
+}
