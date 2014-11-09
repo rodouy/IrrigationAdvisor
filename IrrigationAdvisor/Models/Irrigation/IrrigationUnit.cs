b4bdd661abc41dad74b019beb5d2cd269010b2ce
rodouy <rolivera@outlook.com>
2014-11-08 20:22:22 -0400

    Some lost clases ...

---
+++ IrrigationAdvisor/Models/Irrigation/IrrigationUnit.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IrrigationAdvisor.Models.Utilities;
+using IrrigationAdvisor.Models.Crop;
+
+namespace IrrigationAdvisor.Models.Irrigation
+{
+    /// <summary>
+    /// Create: 2014-10-26
+    /// Author: monicarle
+    /// Description: 
+    ///     Describes an Irrigation Unit
+    ///     
+    /// References:
+    ///     Crop
+    ///     Bomb
+    ///     Location
+    ///     
+    /// Dependencies:
+    ///     CropIrrigationWeather
+    /// 
+    /// TODO: 
+    ///     UnitTest
+    ///     
+    /// -----------------------------------------------------------------
+    /// Fields of Class:
+    ///     - name String
+    /// 
+    /// Methods:
+    ///     - ClassTemplate()      -- constructor
+    ///     - ClassTemplate(name)  -- consturctor with parameters
+    /// 
+    /// </summary>
+    public class IrrigationUnit
+    {
+        #region Consts
+        #endregion
+
+        #region Fields
+        private int id;
+        private String name;
+        private String irrigationType;
+        private double irrigationEfficiency;
+        private List<Pair<DateTime, double>> irrigations;
+        private double surface;
+        private List<Crop.Crop> irrigationCrops;
+        private Bomb bomb;
+        private Location.Location location;
+        #endregion
+
+        #region Properties
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        
+        public String IrrigationType
+        {
+            get { return irrigationType; }
+            set { irrigationType = value; }
+        }
+        
+        public double IrrigationEfficiency
+        {
+            get { return irrigationEfficiency; }
+            set { irrigationEfficiency = value; }
+        }
+        
+        public List<Pair<DateTime, double>> Irrigations
+        {
+            get { return irrigations; }
+            set { irrigations = value; }
+        }
+        
+        public double Surface
+        {
+            get { return surface; }
+            set { surface = value; }
+        }
+        
+        public List<Crop.Crop> IrrigationCrops
+        {
+            get { return irrigationCrops; }
+            set { irrigationCrops = value; }
+        }
+        
+        public Bomb Bomb
+        {
+            get { return bomb; }
+            set { bomb = value; }
+        }
+        
+        public Location.Location Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+        #endregion
+
+        #region Construction
+
+        public IrrigationUnit()
+        {
+            this.Id = 0;
+            this.Name = "noname";
+            this.IrrigationType = "";
+            this.IrrigationEfficiency = 0;
+            this.Irrigations = new List<Pair<DateTime, double>>();
+            this.Surface = 0;
+            this.IrrigationCrops = new List<Crop.Crop>();
+            this.Bomb = new Bomb();
+            this.Location = new Location.Location();
+        }
+        public IrrigationUnit(int pId, String pName, String pIrrigationType,
+            double pIrrigationEfficiency, List<Pair<DateTime, double>> pIrrigations,
+            double pSurface, List<Crop.Crop> pIrrigationCrops, Bomb pBomb, Location.Location pLocation)
+        {
+            this.Id = pId;
+            this.Name = pName;
+            this.IrrigationType = pIrrigationType;
+            this.IrrigationEfficiency = pIrrigationEfficiency;
+            this.Irrigations = pIrrigations;
+            this.Surface = pSurface;
+            this.IrrigationCrops = pIrrigationCrops;
+            this.Bomb = pBomb;
+            this.Location = pLocation;
+        }
+        #endregion
+
+        #region Private Helpers
+        #endregion
+
+        #region Public Methods
+        public bool addIrrigation(DateTime pDateTime, double pValue)
+        {
+            bool lReturn = true;
+            try
+            {
+                Pair<DateTime,double> lPair = new Pair<DateTime,double>(pDateTime,pValue);
+                this.Irrigations.Add(lPair);
+                //return this.Irrigations.Contains(lPair);
+            }
+            catch(Exception e)
+            {
+                lReturn=false;
+                Console.WriteLine("Error in IrrigationUnit.addIrrigation " + e.Message);
+            }
+            return lReturn;
+
+        }
+        public bool  addCrop(Crop.Crop pCrop)
+        {
+            bool lReturn = true;
+            try
+            {
+                this.IrrigationCrops.Add(pCrop);
+                //return this.IrrigationCrops.Contains(pCrop);
+            }
+            catch(Exception e)
+            {
+                lReturn=false;
+                Console.WriteLine("Error in IrrigationUnit.addCrop " + e.Message);
+            }
+            return lReturn;
+        }
+        public double getTotalIrrigation() 
+        {
+            double lReturn = 0;
+            foreach(Pair<DateTime,double> lIrrigation in Irrigations)
+            {
+                lReturn += lIrrigation.Second;
+            }
+            return lReturn;
+        }
+        #endregion
+
+    }
+}
---
+++ IrrigationAdvisor/Models/Water/Irrigation.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IrrigationAdvisor.Models.Water
+{
+        /// <summary>
+        /// Create: 2014-10-30
+        /// Author: monicarle
+        /// Description: 
+        ///     Describes an Irrigation over a Crop
+        ///     
+        /// References:
+        ///     WaterInput
+        ///     
+        /// Dependencies:
+        ///     
+        /// 
+        /// TODO: OK
+        ///     UnitTest
+        ///     
+        /// -----------------------------------------------------------------
+        /// Fields of Class:
+        ///     - type String
+        /// 
+        /// Methods:
+        ///     - Rain()      -- constructor
+        ///     - GetType()
+        /// 
+        /// </summary>
+        public class Irrigation : WaterInput
+        {
+            #region Consts
+
+            private String TYPE = "IRRIGATION";
+
+            #endregion
+
+            #region Fields
+            #endregion
+
+            #region Properties
+            #endregion
+
+            #region Construction
+            /// <summary>
+            /// Constructor of ClassTemplate
+            /// </summary>
+            public Irrigation()
+            {
+
+            }
+
+
+            #endregion
+
+            #region Private Helpers
+            #endregion
+
+            #region Public Methods
+
+            public String getInputType()
+            {
+                return this.TYPE;
+            }
+            #endregion
+
+            #region Overrides
+            #endregion
+
+        
+
+    }
+}
